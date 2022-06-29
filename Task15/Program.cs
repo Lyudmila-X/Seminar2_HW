33acd0e39f72d726fabc70d8d2778c8af8a34d20
Lyudmila <l.xmail@yandex.ru>
2022-06-29 18:18:11 +0300

    Task15 complete, C#, Seminar 2 HW

---
+++ Task15/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+namespace Task15;
+class Days
+{
+    static void Main ()
+    {
+        int WeekDay (int number)
+        {
+            if (number > 7)
+            {
+                if (number % 7 == 0)
+                number = 7;
+                else
+                number = number % 7;       
+            }
+            return number;
+        }
+        string [] days = {"нет", "нет", "нет", "нет", "нет", "да", "да"};
+        Console.WriteLine("Введите цифру: ");
+        int index = WeekDay(Convert.ToInt32(Console.ReadLine()))-1;
+        if (index<0)
+            Console.WriteLine("Ошибка при вводе данных, повторите попытку.");
+        else
+            Console.WriteLine(days[index]);
+    }
+}
