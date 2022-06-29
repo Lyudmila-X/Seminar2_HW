a34759c27c4099f3e682131939d23586d4d6a69e
Lyudmila <l.xmail@yandex.ru>
2022-06-29 17:21:41 +0300

    Task10 complete, C#, Seminar 2 HW

---
+++ Task10/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+namespace Task10;
+class Numbers
+{
+    static void Main ()
+    {
+        Console.WriteLine("Введите число: ");
+        string number = Console.ReadLine()!;
+        char [] array = number.ToCharArray();
+        if (array.Length>1)
+        {
+            Console.WriteLine(array[1]);
+        }
+        else
+            Console.WriteLine("Введенные данные не соответсвуют условиям, попробуйте еще раз");
+    }
+}
